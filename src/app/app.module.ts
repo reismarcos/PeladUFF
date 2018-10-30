79824f2e64b9c833091f209edd25f01f3c5a47fd
reismarcos <reismarcos@id.uff.br>
2018-10-30 12:20:35 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
-
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HistoricoPage } from '../pages/historico/historico';
 import { PerfilPage } from '../pages/perfil/perfil';
 import { CriarPeladaPage } from '../pages/criar-pelada/criar-pelada';
-
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { PeladaService } from './pelada.service';
